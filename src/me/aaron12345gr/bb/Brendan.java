d5d0389beef6ad5a32d5ae471c352c05dd35a27b
aaron12345678890 <72173820+aaron12345678890@users.noreply.github.com>
2020-09-30 20:15:03 -0400

    Create Brendan.java

---
+++ src/me/aaron12345gr/bb/Brendan.java
@@ -0,0 +1,27 @@
+package me.aaron12345gr.bb;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.utils.Compression;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+public class Brendan {
+
+	public static String prefix = "brendan please ";
+
+	public static void main(String[] args) throws LoginException, InterruptedException {
+		JDABuilder builder = JDABuilder.createDefault("token");
+
+		builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+		builder.setBulkDeleteSplittingEnabled(false);
+		builder.setCompression(Compression.NONE);
+		builder.setActivity(Activity.watching("pog"));
+
+		builder.addEventListeners(new PleaseCommand());
+		builder.build();
+
+	}
+
+}
